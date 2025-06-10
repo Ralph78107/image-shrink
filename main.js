20ad60c347c149009d0964d2ad7b801ac527fcbe
Ralph <ralphcaton@gmail.com>
2025-06-10 18:43:06 +0900

    Add menu: Electron image shrink app

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu, global } = require('electron')
+
+//Set env
+process.env.NODE_ENV = 'development'
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+console.log('process.platform')
 
 let mainWindow
 
@@ -13,4 +19,24 @@ function createMainWindow () {
     mainWindow.loadFile('app/index.html')
 }
 
-app.on('ready', createMainWindow)
+app.on('ready', () => {
+    createMainWindow()
+
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
+    mainWindow.on('ready', () => mainWindow = null)
+})
+
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+            label: 'Quit',
+            accelerator: 'CmdOrCtrl+W',
+            click: () => app.quit()
+            },
+        ],
+    },
+]
